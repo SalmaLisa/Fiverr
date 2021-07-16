cf5d102eb3fe41ea577de6ee619e6c17a4c00c4d
samixp02 <samixp@live.com>
2021-07-16 15:09:13 +0200

    ...

--- src/pages/forum/ForumHome.js
+++ src/pages/forum/ForumHome.js
@@ -23,56 +23,6 @@ const ForumHome = () => {
   const [loading, setLoading] = useState(true);
 
 
-  useEffect(function () {
-
-   
-    getforumCats();
-    getforumSubCats();
-    loadPage();
-    setLoading(false);
-    console.log(forumsubcats);
-   
-    
-  }, []);
-
-
-
-  /* const forums = [
-    {
-      _id: "10eba340-d01c-11eb-b8bc-0242ac130003",
-      title: "Hip Replacement",
-      details: "Operations and surgical procedures",
-      icon: "fas fa-cut fa-1x",
-      forum_categories_id: [
-        "36eb5176-d01c-11eb-b8bc-0242ac130024",
-        "10eba340-nfjc-11eb-b8bc-0242ac130003",
-        "36eb5176-bdhc-11eb-b8bc-0242ac130003",
-      ],
-    },
-    {
-      _id: "36eb5176-d01c-11eb-b8bc-0242ac130003",
-      title: "Knee Problems",
-      details: "Bones, joints and muscles",
-      icon: "fas fa-bone fa-1x",
-      forum_categories_id: [
-        "36eb5176-d01c-11eb-b8bc-0242ac130024",
-        "10eba340-nfjc-11eb-b8bc-0242ac130003",
-        "36eb5176-bdhc-11eb-b8bc-0242ac130003",
-      ],
-    },
-    {
-      _id: "3f9e633a-d01c-11eb-b8bc-0242ac130003",
-      title: "Mirtazapine",
-      details: "Brain and nerves",
-      icon: "fas fa-brain fa-1x",
-      forum_categories_id: [
-        "3f9e633a-d01c-3bgd-b8bc-0242ac130003",
-        "36eb5176-d01c-11eb-b8bc-0242ac130024",
-        "3f9e633a-d01c-11eb-b8bc-0242anh3v003",
-      ],
-    },
-  ]; */
-
 
   const getforumCats = async()=>{
   
@@ -120,38 +70,16 @@ const ForumHome = () => {
     }
   };
 
-  // const loadPage = async () => {
-  //   const apiGetPosts = await fetch("http://localhost:8080/api/postsdata").then(
-  //     (result) => result.json()
-  //   );
+  useEffect(function () {
 
-  //   if (apiGetPosts.length == 0) {
-  //     setLastName("");
-  //     setLastUpdated("");
-  //   } else {
-  //     apiGetPosts.forEach((element) => {
-  //       element.createdAt = new Date(element.createdAt)
-  //         .toString()
-  //         .substring(4, 15);
-
-  //       element.updatedAt = new Date(element.updatedAt)
-  //         .toString()
-  //         .substring(4, 15);
-  //     });
-
-  //     let total = 0;
-  //     apiGetPosts.forEach((element) => {
-  //       total = total + element.userReply.length;
-  //     });
-  //     setPostsResult(apiGetPosts);
-  //     setLastName(apiGetPosts[0].user.name);
-  //     setLastUpdated(apiGetPosts[0].updatedAt);
-  //   }
-  // };
-
-
-  
-
+    getforumSubCats();
+    getforumCats();
+    loadPage();
+    setLoading(false);
+    console.log(forumsubcats);
+   
+    
+  }, []);
 
 
 
